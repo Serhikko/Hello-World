a5284a400723608fd7bdfa854ee1148ae38b4d7c
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-10-07 13:20:44 +0100

    troubleshooting
    C++ compiler wasnt working because it didnt had the right path,after ive putted the right one it used C compiler instead of C++,now fixed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-using namespace std;
 
-int main(){
-    cout << "Hello, World!" << endl
-    return 0;
+int main() {
+    std::cout << "Hello, World!" << std::endl; // prints Hello, World! to the console
+    return 0; // indicates that the program ended successfully
 }
