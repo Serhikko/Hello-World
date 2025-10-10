e8808327a49e58974912dc7a29fcb6960cb180fb
Serhii <SERHII.PRYNKEVYCH25@bathspa.ac.uk>
2025-10-10 13:07:17 +0100

    q

--- Chapter_1_Exercises.cpp
+++ Chapter_1_Exercises.cpp
@@ -1,7 +1,24 @@
- #include <iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    cout << "Hello, World!" << endl
+int main() {
+    int firstNumber = 1;
+    bool iCanCode = true;
+    char hopefulGrade = 'a';
+    double myDecimal = 1.0;
+    string minimalSentance = "y";
+    int keyMash = 13213123;
+    auto mysteryDataType = 5.6f; //Use Google to work this out
+
+    cout << "First number: " << firstNumber << endl;
+    cout << "I can code: " << iCanCode << endl;
+    cout << "Hopeful grade: " << hopefulGrade << endl;
+    cout << "My decimal: " << myDecimal << endl;
+    cout << "Minimal sentance: " << minimalSentance << endl;
+    cout << "Key mash: " << keyMash << endl;
+    cout << "Mystery data type: " << mysteryDataType << endl;
+
+
     return 0;
 }
